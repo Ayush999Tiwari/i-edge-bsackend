5dcfee2b734a02b007c31522516ea847d54bb5f0
Ayush Tiwari <ayvsht999@gmail.com>
2026-07-31 12:52:43 +0530

    add get_db dependency

--- app/database.py
+++ app/database.py
@@ -1,4 +1,8 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession
+)
 from sqlalchemy.orm import declarative_base
 from dotenv import load_dotenv
 import os
@@ -6,6 +10,9 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 # convert postgres:// -> postgresql+asyncpg:// if needed
 DATABASE_URL = DATABASE_URL.replace(
@@ -22,7 +29,13 @@ engine = create_async_engine(
 
 async_session = async_sessionmaker(
     engine,
-    expire_on_commit=False
+    expire_on_commit=False,
+    class_=AsyncSession
 )
 
 Base = declarative_base()
+
+
+async def get_db():
+    async with async_session() as session:
+        yield session
